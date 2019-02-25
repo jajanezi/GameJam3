0e9e47c7e83c0e5dda14835a8bce1d07e923cf3a
Vin Nguyen <vinexi.photo@gmail.com>
2019-02-24 17:53:37 -0700

    Torch
    Added axe and light to first person view. Torch is currently flashing, but will be run on a timer in the next commit. The following commit after, I'll be attempting to add a refuel/recharge to the torch when colliding with the campfire.

---
+++ Assets/Torch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Torch : MonoBehaviour
+{
+    public float duration = 1.0F;
+    public Light lt;
+    float torchTimer;
+    float torchGlow = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float phi = Time.time / duration * 2 * Mathf.PI;
+        float amplitude = Mathf.Cos(phi) * 0.5F + 0.5F;
+        lt.intensity = amplitude;
+    }
+}
